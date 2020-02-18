2a4b326639baa184a1c400f799e222fd0da9766f
jnh277 <johannes.n.hendriks@gmail.com>
2020-02-18 14:53:39 +1300

    moves function plot_trace to helpers

--- helpers.py
+++ helpers.py
@@ -17,6 +17,8 @@
 ###############################################################################
 
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def build_phi_matrix(obs,order,inputs):
     "Builds the regressor matrix"
@@ -64,3 +66,26 @@ def generate_data(no_obs, a, b, sigmae):
         Phi[t - order_max, :] = np.hstack((y[range(t-1, t-order_a-1, -1)], u[range(t-1, t-order_b-1, -1)]))
 
     return y, u, Phi
+
+
+def plot_trace(param,num_plots,pos, param_name='parameter'):
+    """Plot the trace and posterior of a parameter."""
+
+    # Summary statistics
+    mean = np.mean(param)
+    median = np.median(param)
+    cred_min, cred_max = np.percentile(param, 2.5), np.percentile(param, 97.5)
+
+    # Plotting
+    plt.subplot(num_plots, 1, pos)
+    plt.hist(param, 30, density=True);
+    sns.kdeplot(param, shade=True)
+    plt.xlabel(param_name)
+    plt.ylabel('density')
+    plt.axvline(mean, color='r', lw=2, linestyle='--', label='mean')
+    plt.axvline(median, color='c', lw=2, linestyle='--', label='median')
+    plt.axvline(cred_min, linestyle=':', color='k', alpha=0.2, label='95% CI')
+    plt.axvline(cred_max, linestyle=':', color='k', alpha=0.2)
+
+    plt.gcf().tight_layout()
+    plt.legend()
